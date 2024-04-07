960c10f17e5e380cd4697a19419a05d02c46bb00
hanyucd <1807003141@qq.com>
2024-04-07 22:35:44 +0800

    feat: 新增设置按钮-drawer

--- src/layout/components/SettingDrawer/SettingDrawer.vue
+++ src/layout/components/SettingDrawer/SettingDrawer.vue
@@ -1,6 +1,6 @@
 <template>
-  <!-- teleport 它可以将一个组件内部的一部分模板“传送”到该组件的 DOM 结构外层的位置去 -->
-  <teleport to="body">
+  <!-- Teleport 它可以将一个组件内部的一部分模板“传送”到该组件的 DOM 结构外层的位置去 -->
+  <Teleport to="body">
     <div class="fixed top-240px right-0 z-10">
       <n-button type="primary" class="b-rd-tr-0! b-rd-br-0!" size="large">
         <template #icon>
@@ -10,7 +10,7 @@
         </template>
       </n-button>
     </div>
-  </teleport>
+  </Teleport>
 </template>
 
 <script lang="ts" setup>
