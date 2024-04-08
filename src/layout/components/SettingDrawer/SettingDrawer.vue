729908418df402679960173eb94adc783788f36a
hanyucd <1807003141@qq.com>
2024-04-08 16:50:24 +0800

    style: 优化

--- src/layout/components/SettingDrawer/SettingDrawer.vue
+++ src/layout/components/SettingDrawer/SettingDrawer.vue
@@ -2,7 +2,7 @@
   <!-- Teleport 它可以将一个组件内部的一部分模板“传送”到该组件的 DOM 结构外层的位置去 -->
   <Teleport to="body">
     <div class="fixed top-240px right-0 z-10">
-      <n-button type="primary" class="b-rd-tr-0! b-rd-br-0!" size="large">
+      <n-button type="primary" class="b-rd-tr-0! b-rd-br-0!" size="large" @click="handleClick(true)">
         <template #icon>
           <n-icon size="24">
             <SettingOutlined />
@@ -11,10 +11,36 @@
       </n-button>
     </div>
   </Teleport>
+
+  <n-drawer v-model:show="show" width="300">
+    <n-drawer-content>
+      这里是内容区域
+    </n-drawer-content>
+
+    <div class="absolute top-240px right-300px">
+      <!-- 关闭按钮 -->
+      <n-button type="primary" class="b-rd-tr-0! b-rd-br-0!" size="large" @click="handleClick(false)">
+        <template #icon>
+          <n-icon size="24">
+            <CloseOutlined />
+          </n-icon>
+        </template>
+      </n-button>
+    </div>
+  </n-drawer>
 </template>
 
 <script lang="ts" setup>
-import { SettingOutlined } from '@vicons/antd';
+import { SettingOutlined, CloseOutlined } from '@vicons/antd';
+
+const show = ref(false);
+
+/**
+ * 打开、关闭 drawer
+ */
+const handleClick = (val: boolean) => {
+  show.value = val;
+};
 </script>
 
 <style scoped>
